ad12348f8c5fac8b576d1dddd1d2eb8d11571d0a
FraPiperis <FrancescaPiperis_00@outlook.it>
2025-02-22 11:32:23 +0100

    modifiche finite

--- compito.js
+++ compito.js
@@ -1,10 +1,13 @@
 const LinkUtile = "https://jsonplaceholder.typicode.com/users"
 
+let Totale = []
+
 const Utenti = async() => {
     try {
         const response = await fetch(LinkUtile); 
         const PrendiUtenti = await response.json(); 
-        
+        renderUsers(PrendiUtenti); 
+        Totale = PrendiUtenti; 
         console.log(PrendiUtenti); 
       }
       catch (err) {
@@ -19,25 +22,44 @@ const Utenti = async() => {
 
     const tabella = PrendiUtenti.map((user)=> NuovaRiga(user)); 
 
-    renderUsers.append(...tabella); 
+    Risultato.append(...tabella); 
 }
 
 function NuovaRiga({name, username, email}){
-    const RigaUtente = document.createElement("tr")
+    const RigaUtente = document.createElement("tr", "table-light")
 
-     const NameUs = document.createElement("td")
+     const NameUs = document.createElement("td","table-light")
      NameUs.innerText= name;
      RigaUtente.appendChild(NameUs); 
 
-     const UsernameUs = document.createElement("td")
+     const UsernameUs = document.createElement("td", "table-light")
      UsernameUs.innerText= username;
      RigaUtente.appendChild(UsernameUs); 
 
-     const MailUs = document.createElement("td")
+     const MailUs = document.createElement("td", "table-light")
      MailUs.innerText= email; 
      RigaUtente.appendChild(MailUs); 
 
      return RigaUtente;
 }
+
+
+
+function cerca() {
+  const select = document.getElementById("select")
+  const input = document.getElementById("input")
+
+  const selectValue = select.value
+  const inputValue = input.value.toLowerCase()
+
+  const userFilter = Totale.filter( user => {
+     return user[selectValue].toLowerCase().includes(inputValue)
+
+  })
+  renderUsers(userFilter)
+}
+
+//user.name oppure user["name"] > primo caso non è dinamico, secondo caso dinamico
+
 
 Utenti(); 
